e591bd7b21f9410586bab027812d19eedc449b49
Jonathan <jonathanbuko@icloud.com>
2024-08-07 12:43:31 +0200

    simplify tracker script

--- pkg/scripts/minify.sh
+++ pkg/scripts/minify.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-bunx uglify-js --compress --mangle --toplevel --output comments=false,beautify=false -o tracker.min.js tracker.js
+bunx uglify-js tracker.js \
+  --compress \
+  sequences=true,dead_code=true,conditionals=true,booleans=true,unused=true,if_return=true,join_vars=true,drop_console=true \
+  --mangle \
+  toplevel=true \
+  --output \
+  comments=false,beautify=false \
+  -o tracker.min.js
--- pkg/scripts/tracker.js
+++ pkg/scripts/tracker.js
@@ -63,25 +63,6 @@ const trackView = async () => {
 
 trackView();
 
-(() => {
-  let oldPushState = history.pushState;
-  history.pushState = function pushState() {
-    let ret = oldPushState.apply(this, arguments);
-    window.dispatchEvent(new Event("pushstate"));
-    window.dispatchEvent(new Event("locationchange"));
-    return ret;
-  };
-
-  let oldReplaceState = history.replaceState;
-  history.replaceState = function replaceState() {
-    let ret = oldReplaceState.apply(this, arguments);
-    window.dispatchEvent(new Event("replacestate"));
-    window.dispatchEvent(new Event("locationchange"));
-    return ret;
-  };
-
-  window.addEventListener("popstate", () => {
-    window.dispatchEvent(new Event("locationchange"));
-  });
-})();
-window.addEventListener("locationchange", trackView);
+window.addEventListener("pushstate", trackView);
+window.addEventListener("replacestate", trackView);
+window.addEventListener("popstate", trackView);
